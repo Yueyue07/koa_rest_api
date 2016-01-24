e3f7395f4563e87cd7cf321e334e133f7d3d9e6b
Yueyue07 <xww.qyy@gmail.com>
2016-01-24 15:28:27 -0800

    remove app.use(allowedMethods())

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const bearsRouter = require(__dirname + '/routes/bears_routes');
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGOLAB_UR || 'mongodb://localhost/bears_app_dev');
 
-app
-  .use(bearsRouter.routes())
-  .use(bearsRouter.allowedMethods());
+app.use(bearsRouter.routes());
+//route /api included in bears_routes.js file
+
 
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('server up: ' + PORT));
