3b74cfe62bd08250ee5316bc7d6a4f64c88cff1e
Yueyue07 <xww.qyy@gmail.com>
2016-01-24 14:45:04 -0800

    add bear model through mongoose.Schema

---
+++ models/bear.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+var bearSchema = new mongoose.Schema({
+  name:{type:String, default:'Winnie'},
+  flavor:{type:String, default:'honey'},
+  fishPreference: {type:String,default: 'salmons'}
+});
+
+var Bear = module.exports = exports = new bearSchema('Bear', bearSchema);
